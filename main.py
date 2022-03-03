4efb596fdcf31aa1ce450f9efff597c5ba40df3f
Ella Karhi <100384206+ellakarhi@users.noreply.github.com>
2022-03-03 22:48:13 +0000

    Finished lab2

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print (5)
+def part1():
+  #This is for part one: Assigning statements
+
+  n = 42
+  pi = 3.14159
+  tau = pi*2
+  school = "Sault college"
+
+def part2():
+  #This is for part two: Syntax errors and naming variables
+
+  course = "CSD110"        
+  desired_grade = 100     
+  hot2 = "hothot"
+  pi = 3.14159
+
+def part3():
+  #This is for part three: Print statements and string operators
+
+  print("Feeling"+"..."+("Hot"*3)+"!")
+
+def part4():
+  #This is for part 4: Debugging and locating error
+  base = 3000    # in cm
+  height = 1000  # in cm
+  volume = 1/3 * base**2 * height
+  #volume = 3000000000
+  print("The volume of a square pyramid with base " + str(base) + "cm and height " + str(height) + "cm is " + str(volume) + "cm^3")
+
+part4()
